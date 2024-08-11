0467962e560c97bc7be91b320c2191c18f10b4ce
jordanconklin <jordinho@ucla.edu>
2024-08-11 14:04:20 -0700

    set up for testing with groqchat

--- chatbot_config.py
+++ chatbot_config.py
@@ -1,6 +1,7 @@
 import os
 from groq import Groq
 from functions import get_settings
+from langchain_groq import GroqChat
 
 try:
     groq_key = get_settings().groq_api_key
@@ -9,6 +10,12 @@ try:
 
     # Initialize Groq client
     client = Groq(api_key=groq_key)
+
+    groq_chat = GroqChat(
+        api_key=groq_key,
+        model_name="llama3-8b-8192"
+    )
+    
     # print(groq_key)
 except Exception as e:
     print(f"An error occurred: {e}")
--- routers/generate_tutorial.py
+++ routers/generate_tutorial.py
@@ -1,5 +1,5 @@
 import os
-from chatbot_config import client
+from chatbot_config import client, groq_chat
 from fastapi import FastAPI, APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 from langchain.memory.buffer_window import ConversationBufferWindowMemory
@@ -21,70 +21,34 @@ from langchain_core.chat_history import BaseChatMessageHistory
 
 router = APIRouter()
 
+conversational_memory_length = 10
 
-# # In-memory store for session history
-# conversation_histories: Dict[str, ConversationBufferWindowMemory] = {}
+memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
 
-# def get_conversation_history(conversation_id: str) -> BaseChatMessageHistory:
-#     """
-#     This function returns the conversation history if it exists. If it doesn't, create a new one
-#     with conversation_id as the key and a buffer window capacity of 10.
-#     """
-#     if conversation_id not in conversation_histories:
-#         conversation_histories[conversation_id] = ConversationBufferWindowMemory(k=10)
-#     return conversation_histories[conversation_id]
-
-message_buffer = ""
-print("initialized buffer")
 
 @router.post('/generate_tutorial/')
-# @router.post('/generate_tutorial/', response_class=StreamingResponse)
 def generate_tutorial(request: ChatbotRequest):
-
-    # TODO dont really wont global
-    global message_buffer
 
     try:
         # Extract player details
         name = request.player_details.name
         age = request.player_details.age
         position = request.player_details.position
-        conversation_id = f"{name}-{age}-{position}"
-
-        # # Get reference to or create conversation history
-        # memory = get_conversation_history(conversation_id)
-
-        # # Add current user input into the conversation history
-        # memory.add_message({"role": "user", "content": request.prompt})
-        
-        # messages = memory.get_messages()
 
         # Generate chat completion using Groq
         chat_completion = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
-                    # "content": f"Player details: name is {name}, age is {str(age)}, position is {position}. {request.prompt}. Keep response short.",
-                    "content": f"{request.prompt}. # Directly reply to everything before the pound sign and dont reply to anything after this semicolon ; Keep some of this information in mind: {message_buffer}",
+                    "content": f"Player details: name is {name}, age is {str(age)}, position is {position}. {request.prompt}. Keep response short.",
                 }
             ],
-            # messages=messages,
-
             model="llama3-8b-8192",
         )
         tutorial = chat_completion.choices[0].message.content
-
-        # conversation_id = f"{request.player_details.name}-{request.player_details.age}-{request.player_details.position}"
-        # streaming_conversation_chain.generate_response(conversation_id, request.prompt)
-
-        # memory.add_message({"role": "assistant", "content": tutorial})
-
-        if len(message_buffer) > 8000:
-            message_buffer = ""
-        message_buffer += tutorial
-        print(message_buffer)
 
         return {"tutorial": tutorial}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error has occurred: {str(e)}")
+    
