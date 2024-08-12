11ff7c19d81d5a73ff27cfc53a5b6f7daa71f6e2
jordanconklin <jordinho@ucla.edu>
2024-08-11 21:08:22 -0700

    memory store is working, now need token limit

--- routers/generate_tutorial.py
+++ routers/generate_tutorial.py
@@ -8,31 +8,44 @@ from langchain_core.chat_history import (
 )
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.messages import HumanMessage
-
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 
 router = APIRouter()
 
-print("initialized")
-
 store = {}
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant specialized in soccer. Answer all questions to the best of your ability.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in store:
         store[session_id] = InMemoryChatMessageHistory()
     return store[session_id]
 
-with_message_history = RunnableWithMessageHistory(model, get_session_history)
+chain = prompt | model
 
+with_message_history = RunnableWithMessageHistory(chain, get_session_history)
+
+
+print("initialized")
 
 @router.post('/generate_tutorial/')
 def generate_tutorial(request: ChatbotRequest):
     try:
         config = {"configurable": {"session_id": "abc"}}
+        prompt = request.prompt
 
         response = with_message_history.invoke(
-            HumanMessage(content=request.prompt),
-            config=config
+            [HumanMessage(content=prompt)],
+            config=config,
         )
 
         return {"tutorial": response.content}
