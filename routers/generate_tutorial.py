79e06530ed6f2ce7ae00ee99a6c960afe6018041
jordanconklin <jordinho@ucla.edu>
2024-08-17 22:29:39 -0700

    streaming chunking on backend works but frontend still messed up

--- routers/generate_tutorial.py
+++ routers/generate_tutorial.py
@@ -4,37 +4,35 @@ Endpoint listening for POST requests from frontend, handles user questions and u
 memory_store.py to communicate with Llama3
 """
 
-from memory_store import with_message_history
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 from models import ChatbotRequest
 from langchain_core.messages import HumanMessage
+from memory_store import with_message_history
 
-# Initialize router for 'generate_tutorial' endpoint handler
 router = APIRouter()
 
 @router.post('/generate_tutorial/')
-def generate_tutorial(request: ChatbotRequest):
-    '''
-    This decorated function listens for POST requests made to server and returns
-    a Llama3 response based on user input in ChatbotRequest
-    '''
+async def generate_tutorial(request: ChatbotRequest):
     try:
         # TODO make a better session ID for each conversation
         session_id = "user123"
-        
+
         # Session ID in config identifies the user's unique conversation when Runnable is made
         config = {"configurable": {"session_id": session_id}}
         prompt = request.prompt 
 
-        # Llama3 runnable invoked with user question and config
-        response = with_message_history.invoke(
-            [HumanMessage(content=prompt)],
-            config=config,
-        )
+        # Llama3 runnable invoked with user question and config, async streaming response
+        async def generate():
+            async for chunk in with_message_history.astream(
+                [HumanMessage(content=prompt)],
+                config=config,
+            ):
+                print(chunk.content)
+                yield f"data: {chunk.content}\n\n"
 
         # Return Llama3 response as dictionary so frontend can read JSON payload
-        return {"tutorial": response.content}
+        return StreamingResponse(generate(), media_type="text/event-stream")
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error has occurred: {str(e)}")
-    
